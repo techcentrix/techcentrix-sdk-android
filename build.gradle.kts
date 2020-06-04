639daeb6cabc1187ca7f94dde436f53f8695597d
Paweł Marchewka <4911441+pawelmarchewka@users.noreply.github.com>
2020-06-04 11:36:10 +0200

    Update Android Studio to version 4.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:3.6.3")
+        classpath("com.android.tools.build:gradle:4.0.0")
         classpath(kotlin("gradle-plugin", version = "1.3.72"))
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
